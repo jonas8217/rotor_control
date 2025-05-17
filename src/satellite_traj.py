c6e1b2db66f740ebe9cc4ea6b445ecb3641f039d
jonas8217 <jonas82173000@gmail.com>
2025-05-17 23:08:00 +0200

    update to trajectory planning

--- src/satellite_traj.py
+++ src/satellite_traj.py
@@ -1,7 +1,9 @@
+from pyorbital import orbital
+import os.path
+from datetime import datetime,timedelta
+import numpy as np
 
 def main(argv):
-    from pyorbital import orbital
-    import os.path
 
     if len(argv) < 2:
         print("No sattelite name given")
@@ -16,62 +18,108 @@ def main(argv):
         return
 
     # from pyorbital import tlefile
-    from datetime import datetime,timedelta
-    import numpy as np
 
 
-    # Ground station coordinates provided from google maps
+    # Ground station coordinates gathered from google maps
     # Altitude gotten from en-gb.topographic-map.com/map-z61h/Denmark/ and www.freemaptools.com/elevation-finder.htm
     # building height estimated by pythagoran therom
     # TODO possibly dynamically load
     latitude,longitude = 55.3675462, 10.4318222
     building_height = 10
-    ground_altitude = 21
+    ground_altitude = 21 # compared to sea level
+
+    rotor_min_el = 0
+    rotor_max_el = 180 # TODO: Verify
+    rotor_min_az = -360 # TODO: Test
+    rotor_max_az = 360 # TODO: Test
+
+    rotor_az_max_speed = 2 # degrees per second
+    rotor_el_max_speed = 2 # degrees per second
+
 
     hours = 24
 
     pass_times = orb.get_next_passes(datetime.now(), hours, longitude, latitude, ground_altitude + building_height)
 
+    min_elevation = 15
+
     for p in pass_times:
         rise,fall,max_el = p
         
-        delta = fall-rise
-        # print(delta.seconds)
-        # print(dir(delta))
+        if orb.get_observer_look(max_el, longitude, latitude, ground_altitude + building_height)[1] < min_elevation:
+            continue
+
+        pass_time = fall-rise
+        # print(pass_time.total_seconds())
+        # print(dir(pass_time))
         # exit()
 
-        points = 100 # points per second
+        point = 100 # total trajectory points
 
-        azs,els = [],[]
+        ts,azs,els = [],[],[]
 
-        for i in range(points):
-            t = rise + timedelta(seconds=i * delta.seconds / points)
-            az,el = orb.get_observer_look(t, longitude, latitude, ground_altitude + building_height)
+        for i in range(point):
+            dt = timedelta(seconds=i * pass_time.total_seconds() / point)
+            az,el = orb.get_observer_look(rise + dt, longitude, latitude, ground_altitude + building_height)
+            ts.append(dt.total_seconds())
             azs.append(float(az))
             els.append(float(el))
 
         # print(azs)
         # print(els)
 
+        az_dots,el_dots = [],[]
+        t_prev,az_prev,el_prev = ts[0],azs[0],els[0]
+        for t,az,el in zip(ts[1:], azs[1:], els[1:]):
+            az_dots.append((az-az_prev)/(t-t_prev))
+            el_dots.append((el-el_prev)/(t-t_prev))
 
+        az_dots.append(az_dots[-1])
+        el_dots.append(el_dots[-1])
+
+        # print(az_dots)
+
+        az_too_fast,el_too_fast = [],[]
+        for az,el,az_dot,el_dot in zip(azs,els,az_dots,el_dots):
+            if abs(az_dot) > rotor_az_max_speed:
+                az_too_fast.append([az,el])
+            if abs(el_dot) > rotor_el_max_speed:
+                el_too_fast.append([az,el])
+        
         if True:
         
             xs = np.array([azs,els])
 
-            # print(xs.shape)
-            # exit()
+            xs_fast_az = np.array(az_too_fast).T
+            xs_fast_el = np.array(el_too_fast).T
             
             from matplotlib import pyplot as plt
+            # from matplotlib.colors import ListedColormap, BoundaryNorm
 
-            # fig = plt.figure()
+            print(xs_fast_az.shape,xs_fast_el.shape)
 
-            # ax = fig.add_subplot(projection="3d")
             fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
             # ax.axis("equal")
-            ax.plot(xs[0,:]*np.pi/180, 90-xs[1,:], marker=" ") # TODO
+            ax.set_rlim(bottom=90, top=0)
+            ax.plot(xs[0,:]*np.pi/180, xs[1,:], marker=" ")
             # ax.scatter(0,0,0, marker="X")
+            if xs_fast_az.size != 0:
+                ax.plot(xs_fast_az[0,:]*np.pi/180, xs_fast_az[1,:], marker=" ")
+            if xs_fast_el.size != 0:
+                ax.plot(xs_fast_el[0,:]*np.pi/180, xs_fast_el[1,:], marker=" ")
 
             plt.show()
+
+        ans = input("Create trajectory? [y/N]")
+        if ans.lower() != "y":
+            continue
+
+        # create trajectory
+        # visualize problem areas?
+        #   angular velocity greater than combined axes  velocity limits 
+
+        
+
 
 
 if False:
