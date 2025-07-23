6f967e5023782535a6a625bd0162e606eb61b595
jonas8217 <jonas82173000@gmail.com>
2025-07-24 00:12:47 +0200

    Updated the script to use a more accurate geographic coordiante to ECEF frame convertion method. Added functionality for tilt and heading correction. Switched over to XY pointing instead of Azimuth Elevation to directly align with the ground station rotor configuration

--- src/balloon_pointer.py
+++ src/balloon_pointer.py
@@ -10,61 +10,44 @@ import json
 
 # lesson learned: inventory
 
-def earth_R(lat: float): # input radians
-    R_eq = 6378137 # radius at equator
-    R_p = 6356752 # radius at the poles
-    f = (R_eq - R_p) / R_eq
-    return R_eq * (1 - f*np.sin(lat)**2)
+def geo_to_ecef(lat, lon, alt):
+    rad_lat = lat * (np.pi / 180.0)
+    rad_lon = lon * (np.pi / 180.0)
 
+    a = 6378137.0
+    finv = 298.257223563
+    f = 1 / finv
+    e2 = 1 - (1 - f) * (1 - f)
+    v = a / np.sqrt(1 - e2 * np.sin(rad_lat) * np.sin(rad_lat))
 
-def geo2vec(lat : float, long : float): # input degrees
-    r = earth_R(lat)
-    la = (90-lat)*np.pi/180
-    lo = long*np.pi/180
     return np.array([
-        r * np.sin(la) * np.cos(lo),
-        r * np.sin(la) * np.sin(lo),
-        r * np.cos(la)
+        (v + alt) * np.cos(rad_lat) * np.cos(rad_lon),
+        (v + alt) * np.cos(rad_lat) * np.sin(rad_lon),
+        (v * (1 - e2) + alt) * np.sin(rad_lat)
     ])
 
-def vec2geo(v):
+def vec2XY(v): # pointing vector to XY
+    v = v/np.linalg.norm(v)
     return (
-        float(-np.arccos(v[2]/np.linalg.norm(v)) * 180/np.pi + 90),
-        float(np.atan2(v[1],v[0]) * 180/np.pi)
+        float(np.arcsin(-v[1]) * 180/np.pi),
+        float(np.arcsin(v[0] / np.sqrt(1-v[1]*v[1] + 1e-20)) * 180/np.pi)
     )
-
-def vec2AzEl(v):
-    res = vec2geo(v)
-    return (-res[1], res[0])
-
-def vec2XY():
-    pass
-
-def gcd_arc(geo1 : tuple[float, float], geo2 : tuple[float, float]):
-    v1 = geo2vec(*geo1)
-    v2 = geo2vec(*geo2)
-    theta = np.arccos(np.dot(v1,v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))
-    # using the average latitude of the two geographic coordinates to estimate the radius of the earth in that region. 
-    # This idea is flawed if the shortest path does not cross this average latitude. Eg. when the shortest distance crosses near one of the poles.
-    # A better method could involve using half the vector between the two gepgraphic points to re-project onto the sphere, thus finding the real center point between the geographic coordinates
-    return theta * earth_R((geo1[0]+geo2[0])/2)
-
 
 # Rotation matricies
 R_x = lambda a: np.array([
-    [1,           0,           0],
+    [1,         0,         0],
     [0, np.cos(a),-np.sin(a)],
     [0, np.sin(a), np.cos(a)]
 ])
 R_y = lambda a: np.array([
     [ np.cos(a), 0, np.sin(a)],
-    [           0, 1,           0],
+    [         0, 1,         0],
     [-np.sin(a), 0, np.cos(a)]
 ])
 R_z = lambda a: np.array([
     [np.cos(a),-np.sin(a), 0],
     [np.sin(a), np.cos(a), 0],
-    [0,           0,           1]
+    [0,         0,         1]
 ])
 
 # scew-matrix operator
@@ -84,9 +67,12 @@ xyz2yzx = np.array([
     [0,1,0]
 ])
 
-def world2gs(lat : float, long : float, az_offset: float = 0): # rotation matrix from geographic coordinate system world frame to ground station frame
-    return R_z(long*np.pi/180) @ R_y(-lat*np.pi/180) @ xyz2yzx @ R_z(90*np.pi/180) @ R_z(-az_offset*np.pi/180)
+
+def world2gs(lat : float, long : float): # rotation matrix from geographic coordinate system world frame to ground station frame
+    return R_z(long*np.pi/180) @ R_y(-lat*np.pi/180) @ xyz2yzx @ R_z(90*np.pi/180)
     
+def gs_tilt_correction(az_offset : float, tilt_axis_angle : float, tilt : float):
+    return R_z(tilt_axis_angle*np.pi/180) @ R_y(tilt*np.pi/180) @ R_z(-az_offset*np.pi/180)
 
 def main(argv : list[str]):
     # load ground station config
@@ -98,32 +84,30 @@ def main(argv : list[str]):
     long_gs = data["longitude"]
     alt_gs = data["altitude"]
     az_offset = data["azimuth_offset"]
+    tilt_axis = data["tilt"]
+    tilt = data["tilt_axis"]
 
     # Get the balloon coordinates from the input arguments
-    geo_b = (float(argv[1]),float(argv[2]))
-    alt_b = float(argv[3])
+    lat_b,long_b,alt_b = (float(argv[1]),float(argv[2]),float(argv[3]))
 
-    # Calculate the vectors  
-    v_gs = geo2vec(lat_gs,long_gs)
-    v_b = geo2vec(*geo_b)
- 
-    # Extend vectors by their altitudes
-    extend = lambda vec,a : (1+a/np.linalg.norm(vec))*vec
-    v_gs = extend(v_gs,alt_gs)
-    v_b = extend(v_b,alt_b)
+    # Calculate the vectors (Earth Centered Earth Fixed frame)
+    v_gs = geo_to_ecef(lat_gs,long_gs,alt_gs)
+    v_b = geo_to_ecef(lat_b,long_b,alt_b)
+
+    print(tuple(map(float,v_gs/1000000)))
+    print(tuple(map(float,v_b/1000000)))
 
     # get the pointing vector between the ground station and the balloon
     v_point = v_b - v_gs
-    v_point = v_point/np.linalg.norm(v_point)
+    dist = np.linalg.norm(v_point)
+    v_point = v_point/dist
 
     # get the pointing vector in the ground station frame
-    gs_v_point = np.transpose(world2gs(lat_gs,long_gs,az_offset)) @ v_point
-
+    gs_v_point = np.transpose(world2gs(lat_gs, long_gs) @ gs_tilt_correction(az_offset, tilt_axis, tilt)) @ v_point
 
     # calculate the azimuth and elevation from the vector in the ground station frame
-    az,el = vec2AzEl(gs_v_point)
-    print(az,el)
-
+    X,Y = vec2XY(gs_v_point)
+    print(X,Y, dist)
 
 
 if __name__ == "__main__":
--- src/get_rotor_speed.py
+++ src/get_rotor_speed.py
@@ -22,18 +22,28 @@ for file_name in file_names:
     table = np.array(table)
 
     if abs(table[0,3] - table[-1,3]) > 0.2:
-        az_init = v = table[0,3]
+        az_init = table[0,3]
         i = 0
-        while az_init - table[i,3]:
+        while not az_init - table[i,3]:
             i += 1
         
         az_speed = (table[-1,3] - table[i,3])/(table[-1,0]-table[i,0])
         print("az_speed", az_speed)
     else:
-        el_init = v = table[0,4]
+        el_init = table[0,4]
         i = 0
-        while el_init - table[i,4]:
+        while not el_init - table[i,4]:
             i += 1
         
         el_speed = (table[-1,4] - table[i,4])/(table[-1,0]-table[i,0])
         print("el_speed", el_speed)
+
+# el_speed = az_speed = 0
+# el_speed += abs(1.5474773986052)
+# el_speed += abs(-1.5220002383855793)
+# az_speed += abs(-1.5332449128253802)
+# az_speed += abs(1.5230980871106503)
+# el_speed += abs(1.4471889478915907)
+# az_speed += abs(-1.555581053185235)
+# az_speed += abs(1.508936130978093)
+# print(el_speed/3,az_speed/4)
